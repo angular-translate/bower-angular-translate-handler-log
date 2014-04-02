0b743891c433afcf565ef5883f150a03f68b099f
Pascal Precht <pascal.precht@googlemail.com>
2014-04-02 21:44:32 +0200

    updates package

--- angular-translate-handler-log.js
+++ angular-translate-handler-log.js
@@ -1,3 +1,8 @@
+/*!
+ * angular-translate - v2.0.1 - 2014-04-02
+ * http://github.com/PascalPrecht/angular-translate
+ * Copyright (c) 2014 ; Licensed MIT
+ */
 angular.module('pascalprecht.translate').factory('$translateMissingTranslationHandlerLog', [
   '$log',
   function ($log) {
